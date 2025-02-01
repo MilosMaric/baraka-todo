fa133100b26c4d0acc3379bc8a38884cef5e4cec
Milos Maric <milososig@gmail.com>
2025-02-01 19:41:04 +0100

    Switch to HTTPS for the Quotes API

--- src/utils/fetchQuote.ts
+++ src/utils/fetchQuote.ts
@@ -2,7 +2,7 @@ const quoteApiUrlEnv = process.env.QUOTE_API_URL
 
 export const fetchQuote = async () => {
   const response = await fetch(
-    quoteApiUrlEnv ?? 'http://api.quotable.io/random?tags=motivational&limit=10'
+    quoteApiUrlEnv ?? 'https://api.quotable.io/random?tags=motivational&limit=10'
   )
   const data = await response.json()
   return data.content
